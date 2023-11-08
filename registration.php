813ebf40507250fb101b43f901b38fe234bec060
Ashok_dobariya <ashokpdobariya2081978@gmail.com>
2023-11-08 15:13:28 +0530

    Intial update

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Amasty Team
+ * @copyright Copyright (c) 2022 Amasty (https://www.amasty.com)
+ * @package OSC ThankYouPage Hyva Compatibility by Amasty
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Zealousecommerce_QtymanagerHyvaCompatibility',
+    __DIR__
+);
